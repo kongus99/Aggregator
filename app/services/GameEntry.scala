aa4034f3c2c49d58f8201233a9ede27244a1b579
kongus99 <kongus99@gmail.com>
2016-11-28 15:33:35 +0100

    inclusion of match data in main view - steam duplicates included

--- app/services/GameEntry.scala
+++ app/services/GameEntry.scala
@@ -3,6 +3,7 @@ package services
 import model.Tables
 import play.api.libs.functional.syntax._
 import play.api.libs.json.{JsPath, Writes}
+import services.GameOn.GameOn
 
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -35,16 +36,17 @@ object GameEntry {
       steam <- tables.getSteamEntries
       matches <- tables.getAllMatches
     } yield {
+      val gogMap = gog.map(e => (e.gogId, e)).toMap
+      val steamMap = steam.map(e => (e.steamId, e)).toMap
+      def simplify(p: ((GameOn, Long), (GameOn, Long))) = if (p._1._1 == GameOn.Gog || (p._1._1 == GameOn.Steam && p._2._1 == GameOn.Steam && p._1._2 < p._2._2)) p.swap else p
+      val simplified = matches.toSeq.flatMap(m => m._2.map(p => ((m._1._1, p._1), (m._1._2, p._2)))).map(simplify).distinct
+      val mappedBySteamId = simplified.map(p => (p._1._2, p._2)).groupBy(_._1).mapValues(_.map(_._2))
+      val repeated = mappedBySteamId.toSeq.map(p => (GameOn.Steam, p._1) +: p._2).map(s => s.partition(_._1 == GameOn.Steam)).map(p => GameEntry(p._2.map(e => gogMap(e._2)), p._1.map(e => steamMap(e._2))))
       val repeatingGogIds = matches.filter(p => p._1._1 == GameOn.Gog).flatMap(_._2).keys.toSet
       val repeatingSteamIds = matches.filter(p => p._1._1 == GameOn.Steam).flatMap(_._2).keys.toSet
-      val repeatingGogEntries = gog.filter(g => repeatingGogIds.contains(g.gogId))
-      val repeatingSteamEntries = steam.filter(s => repeatingSteamIds.contains(s.steamId))
-      val both = repeatingGogEntries.flatMap(g => repeatingSteamEntries.map(s => (g, s)))
-        .filter({case (g, s) => matches.get((GameOn.Gog, GameOn.Steam)).exists(set => set.contains(g.gogId, s.steamId))})
-        .map({case (g, s) => GameEntry(Seq(g), Seq(s))})
       val onlyGog = gog.filter(g => !repeatingGogIds.contains(g.gogId)).map(g => GameEntry(Seq(g), Seq()))
       val onlySteam = steam.filter(g => !repeatingSteamIds.contains(g.steamId)).map(s => GameEntry(Seq(), Seq(s)))
-      (both ++ onlyGog ++ onlySteam).sortBy(_.name)
+      (repeated ++ onlyGog ++ onlySteam).sortBy(_.name)
     }
   }
 }
