b11ccba54bfd32b5f1c0cfba08eef56e32b63232
kongus99 <kongus99@gmail.com>
2016-12-01 15:09:11 +0100

    gog prices are read

--- app/services/GogEntry.scala
+++ app/services/GogEntry.scala
@@ -2,7 +2,7 @@ package services
 
 import model.Tables
 import play.api.libs.functional.syntax._
-import play.api.libs.json.{JsPath, Json, Reads, Writes}
+import play.api.libs.json._
 import services.GameEntry._
 import services.GameSources.GameSources
 
@@ -13,22 +13,28 @@ case class GogEntry(title: String, gogId : Long, price : Option[Float] = None)
 object GogEntry{
   private val regExp = "var gogData = (.+);".r
 
-  implicit val gogReads: Reads[GogEntry] = ((JsPath \ "title").read[String] and (JsPath \ "id").read[Long])((t, i) => GogEntry(t, i))
+  private val coreGogRead = (JsPath \ "title").read[String] and (JsPath \ "id").read[Long]
+  val gogWishListReads: Reads[GogEntry] = (coreGogRead and (JsPath \ "price" \ "finalAmount").read[String])((t, i, p) => GogEntry(t, i, Some(p.toFloat)))
+  val gogReads: Reads[GogEntry] = coreGogRead((t, i) => GogEntry(t, i))
+
   implicit val gogWrites: Writes[GogEntry] = (
       (JsPath \ "title").write[String] and
       (JsPath \ "gogId").write[Long] and
       (JsPath \ "price").write[Option[Float]])((e) => (e.title, e.gogId, e.price))
 
   private def parseWishList(wishList: String) = {
-    parseGogEntries(regExp.findAllMatchIn(wishList).map(m => m.group(1)).next()).map(_.copy(price = Some(1.0f)))
+    parseGogEntries(gogWishListReads)(regExp.findAllMatchIn(wishList).map(m => m.group(1)).next())
   }
 
   def getFromGog(tables : Tables)(owned: Seq[String], wishList : String, sources : GameSources)(implicit exec: ExecutionContext): Future[Seq[GameEntry]] = {
-    val parsed = owned.flatMap(parseGogEntries) ++ parseWishList(wishList)
+    val parsed = owned.flatMap(parseGogEntries(gogReads)) ++ parseWishList(wishList)
     tables.replaceGogData(parsed).flatMap(_ => generateFromNames(sources, tables))
   }
 
   def getGogPageNumber(body : String) : Int = (Json.parse(body) \ "totalPages").as[Int]
 
-  private def parseGogEntries(body: String) = (Json.parse(body) \ "products").validate[List[GogEntry]].get
+  private def parseGogEntries(reads : Reads[GogEntry])(body: String) = {
+    val parse = Json.parse(body)
+    (parse \ "products").as[List[JsValue]].map(_.as(reads))
+  }
 }
