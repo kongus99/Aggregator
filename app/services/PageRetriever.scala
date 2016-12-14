cfd1d7985def97a0273245057f62e7339c1d9325
kongus99 <kongus99@gmail.com>
2016-12-14 15:12:37 +0100

    refactoring

--- app/services/PageRetriever.scala
+++ app/services/PageRetriever.scala
@@ -7,45 +7,45 @@ import scala.collection.immutable.Seq
 import scala.concurrent.{ExecutionContext, Future}
 
 abstract class PageRetriever(client: WSClient)(implicit exec: ExecutionContext) {
-  def address(user : String)(query : String): UrlAddress
+  def address(user: String)(query: String): UrlAddress
 
   def retrieve(query: String): Future[String] = retrieveWithUser("")(query)
 
-  def retrieveWithUser(user : String)(query: String): Future[String] = {
+  def retrieveWithUser(user: String)(query: String): Future[String] = {
     val add = address(user)(query)
-    client.url(add.url).withHeaders(add.headers : _*).get().map(_.body)
+    client.url(add.url).withHeaders(add.headers: _*).get().map(_.body)
   }
 }
 
-class GogPageRetriever(client: WSClient, configuration: Configuration)(implicit exec: ExecutionContext){
+class GogPageRetriever(client: WSClient, configuration: Configuration)(implicit exec: ExecutionContext) {
   lazy val address = UrlAddress(
     "https://www.gog.com/account/getFilteredProducts?hiddenFlag=0&mediaType=1",
-    ("Cookie", configuration.underlying.getString("gog.cookies"))::Nil)
+    ("Cookie", configuration.underlying.getString("gog.cookies")) :: Nil)
 
-  def retrieve(pageNumberParser : String => Int): Future[Seq[String]] = {
-    client.url(address.url).withHeaders(address.headers : _*).get().map(_.body).map(pageNumberParser).flatMap(retrievePages)
-  }
+  def retrieve(pageNumberParser: String => Int): Future[Seq[String]] = {
+    def retrievePages(number: Int): Future[Seq[String]] = {
+      val addresses = (1 to number).map(address.url + "&page=" + _)
+      Future.sequence(addresses.map(client.url(_).withHeaders(address.headers: _*).get().map(_.body)))
+    }
 
-  private def retrievePages(number: Int): Future[Seq[String]] = {
-    val addresses = (1 to number).map(address.url + "&page=" + _)
-    Future.sequence(addresses.map(client.url(_).withHeaders(address.headers: _*).get().map(_.body)))
+    client.url(address.url).withHeaders(address.headers: _*).get().map(_.body).map(pageNumberParser).flatMap(retrievePages)
   }
 }
 
 class GogWishListRetriever(client: WSClient, configuration: Configuration)(implicit exec: ExecutionContext) extends PageRetriever(client) {
-  override def address(user: String)(query: String): UrlAddress = UrlAddress(s"https://www.gog.com/u/$user$query", Seq())
+  override def address(user: String)(query: String): UrlAddress = UrlAddress(s"https://www.gog.com/u/$user$query")
 }
 
 class SteamPageRetriever(client: WSClient)(implicit exec: ExecutionContext) extends PageRetriever(client) {
-  override def address(user: String)(query: String): UrlAddress = UrlAddress(s"http://steamcommunity.com/id/$user$query", Seq())
+  override def address(user: String)(query: String): UrlAddress = UrlAddress(s"http://steamcommunity.com/id/$user$query")
 }
 
 class ReferenceRatesRetriever(client: WSClient)(implicit exec: ExecutionContext) extends PageRetriever(client) {
-  override def address(user: String)(query: String): UrlAddress = UrlAddress("http://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml", Seq())
+  override def address(user: String)(query: String): UrlAddress = UrlAddress("http://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml")
 }
 
 class GolRetriever(client: WSClient)(implicit exec: ExecutionContext) extends PageRetriever(client) {
-  override def address(user: String)(query: String): UrlAddress = UrlAddress("http://www.gry-online.pl" + query, Seq())
+  override def address(user: String)(query: String): UrlAddress = UrlAddress("http://www.gry-online.pl" + query)
 }
 
 class FKRetriever(client: WSClient)(implicit exec: ExecutionContext) extends PageRetriever(client) {
@@ -53,9 +53,10 @@ class FKRetriever(client: WSClient)(implicit exec: ExecutionContext) extends Pag
     ("X-Requested-With", "XMLHttpRequest") ::
     ("Referer", "http://www.fabrykakluczy.pl/") ::
     Nil
+
   override def address(user: String)(query: String): UrlAddress = UrlAddress("http://www.fabrykakluczy.pl" + query, header)
 }
 
 class KeyeRetriever(client: WSClient)(implicit exec: ExecutionContext) extends PageRetriever(client) {
-  override def address(user : String)(query : String) = UrlAddress("https://www.keye.pl" + query, Seq())
+  override def address(user: String)(query: String) = UrlAddress("https://www.keye.pl" + query)
 }
