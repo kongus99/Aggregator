b995360cd57ba333df8b7ce905ac93f1ca92051e
kongus99 <kongus99@gmail.com>
2017-01-23 15:55:06 +0100

    cleanup

--- app/modules/ScheduleActor.scala
+++ app/modules/ScheduleActor.scala
@@ -2,7 +2,7 @@ package modules
 
 import akka.actor.{Actor, ActorSystem}
 import com.google.inject.Inject
-import model.{CurrencyConverter, Tables, User}
+import model.{CurrencyConverter, Tables}
 import modules.ScheduleActor.{RefreshGog, RefreshPrices, RefreshSteam}
 import play.api.Configuration
 import play.api.libs.ws.WSClient
@@ -30,59 +30,49 @@ class ScheduleActor @Inject()(actorSystem: ActorSystem, client: WSClient, config
   val keyeRetriever = new KeyeRetriever(client)
 
   override def receive: Receive = {
-    case _ : RefreshSteam => {
-      val f = for {
-        user <- tables.getUserById(1L)
-        rates <- ratesRetriever.retrieve("").map(CurrencyConverter.parseFromXml)
-        (steamOwned, steamWishlist) <- getSteamGames(user)
-        refreshed <- tables.replaceSteamData(user, SteamEntry.parse(steamOwned, steamWishlist, rates))
-      } yield {
-        refreshed
-      }
-      Await.result(f, Duration.Inf)
+    case _ : RefreshSteam =>
+      Await.result(refreshSteamGames(1L), Duration.Inf)
       println("Refreshed steam")
-    }
-    case _ : RefreshGog => {
-      val f = for {
-        user <- tables.getUserById(1L)
-        rates <- ratesRetriever.retrieve("").map(CurrencyConverter.parseFromXml)
-        (gogOwned, gogWishlist) <- getGogGames(user)
-        refreshed <- tables.replaceGogData(user, GogEntry.parse(gogOwned, gogWishlist, rates))
-      } yield {
-        refreshed
-      }
-      Await.result(f, Duration.Inf)
+    case _ : RefreshGog =>
+      Await.result(refreshGogGames(1L), Duration.Inf)
       println("Refreshed gog")
-    }
-    case _ : RefreshPrices => {
-      val f = for {
-        user <- tables.getUserById(1L)
-        prices <- PriceEntry.getPrices(tables, user, golRetriever.retrieve, fkRetriever.retrieve, keyeRetriever.retrieve)
-        refreshed <- tables.replacePrices(prices.values.flatten.toSeq)
-      } yield {
-        refreshed
-      }
-      Await.result(f, Duration.Inf)
+    case _ : RefreshPrices =>
+      Await.result(refreshPrices(1L), Duration.Inf)
       println("Refreshed prices")
-    }
-
   }
 
-  private def getGogGames(user: Option[User]) = {
+  private def refreshGogGames(userId: Long): Future[Any] = {
     for {
+      user <- tables.getUserById(userId)
+      rates <- ratesRetriever.retrieve("").map(CurrencyConverter.parseFromXml)
       owned <- gogRetriever.retrieve(getGogPageNumber)
       wishlist <- user.map(u => u.gogLogin.map(l => gogWishListRetriever.retrieveWithUser(useAlternate = false)(l)("/wishlist")).getOrElse(Future(""))).getOrElse(Future(""))
+      refreshed <- tables.replaceGogData(user, GogEntry.parse(owned, wishlist, rates))
     } yield {
-      (owned, wishlist)
+      refreshed
     }
   }
 
-  private def getSteamGames(user: Option[User]) = {
+  private def refreshSteamGames(userId: Long): Future[Any] = {
     for {
+      user <- tables.getUserById(userId)
+      rates <- ratesRetriever.retrieve("").map(CurrencyConverter.parseFromXml)
       owned <- user.map(u => u.steamLogin.map(l => steamRetriever.retrieveWithUser(u.steamAlternate)(l)("/games/?tab=all")).getOrElse(Future(""))).getOrElse(Future(""))
       wishlist <- user.map(u => u.steamLogin.map(l => steamRetriever.retrieveWithUser(u.steamAlternate)(l)("/wishlist")).getOrElse(Future(""))).getOrElse(Future(""))
+      refreshed <- tables.replaceSteamData(user, SteamEntry.parse(owned, wishlist, rates))
+
     } yield {
-      (owned, wishlist)
+      refreshed
+    }
+  }
+
+  private def refreshPrices(userId: Long) : Future[Any] ={
+    for {
+      user <- tables.getUserById(userId)
+      prices <- PriceEntry.getPrices(tables, user, golRetriever.retrieve, fkRetriever.retrieve, keyeRetriever.retrieve)
+      refreshed <- tables.replacePrices(prices.values.flatten.toSeq)
+    } yield {
+      refreshed
     }
   }
 
