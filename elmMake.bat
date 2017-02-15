df91f3a94d9c0165041714615101ccd0e4a48fe1
kongus99 <kongus99@gmail.com>
2017-02-15 16:40:16 +0100

    search input now sends request for new search results

--- elmMake.bat
+++ elmMake.bat
@@ -1,2 +1,5 @@
 elm-format src/ --yes
+elm package install
+elm make src/Login.elm --output public/javascripts/login.js
 elm make src/MainPage.elm --output public/javascripts/mainPage.js
+elm make src/Comparison.elm --output public/javascripts/comparison.js
