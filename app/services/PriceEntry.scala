9b917322b3ec7c48bb4126cc83f1217ea07b5999
kongus99 <kongus99@gmail.com>
2017-02-20 15:18:35 +0100

    refactoring

--- app/services/PriceEntry.scala
+++ app/services/PriceEntry.scala
@@ -55,14 +55,14 @@ object KeyePricesFetcher {
   private def autoCompleteUrl(query: String) = s"/index/lists?term=$query"
 
   def getPrices(entries: Seq[SteamEntry], tables: Tables, retriever: (String) => Future[String])(implicit exec: ExecutionContext): Future[Seq[PriceEntry]] = {
+    def getWinner(steamEntry: SteamEntry, suggestions: Seq[PartialPrice]) = suggestions.headOption
+
+    def parsePrice(steamEntry: SteamEntry, suggestions: Seq[PartialPrice]): Option[PriceEntry] =
+      getWinner(steamEntry, suggestions).map(d => PriceEntry(steamEntry.steamId, d.name, "https://" + Keye.toString, "https://" + Keye.toString + d.link, BigDecimal(d.price.get).setScale(2)))
+
     for {
       complete <- Future.sequence(entries.map(e => getSuggestions(e.name, tables, retriever).map(s => (e, s))))
     } yield {
-      def parsePrice(steamEntry: SteamEntry, suggestions: Seq[PartialPrice]): Option[PriceEntry] = {
-        val data = suggestions.headOption
-        data.map(d => PriceEntry(steamEntry.steamId, d.name, "https://" + Keye.toString, "https://" + Keye.toString + d.link, BigDecimal(d.price.get).setScale(2)))
-      }
-
       complete.filter(p => p._2.nonEmpty).map((parsePrice _).tupled).filter(_.isDefined).map(_.get)
     }
   }
@@ -83,23 +83,18 @@ object FKPricesFetcher {
   private def autoCompleteUrl(query: String) = s"/search/?q=$query"
 
   def getPrices(entries: Seq[SteamEntry], tables: Tables, retriever: (String) => Future[String])(implicit exec: ExecutionContext): Future[Seq[PriceEntry]] = {
-    def getLinks = {
-      for {
-        complete <- Future.sequence(entries.map(e => getSuggestions(e.name, tables, retriever).map(s => (e, s))))
-      } yield {
-        def parseUrl(steamEntry: SteamEntry, suggestions: Seq[PartialPrice]): Option[PriceEntry] = {
-          suggestions.map(s => (ThresholdLevenshtein.count(s.name, steamEntry.name, 100), s)).sortBy(_._1).headOption.map(_._2).map(p => PriceEntry(steamEntry.steamId, p.name, FK.toString, p.link, BigDecimal(0)))
-        }
+    def getWinner(steamEntry: SteamEntry, suggestions: Seq[PartialPrice]) =
+      suggestions.map(s => (ThresholdLevenshtein.count(s.name, steamEntry.name, 100), s)).sortBy(_._1).headOption.map(_._2)
 
-        complete.filter(p => p._2.nonEmpty).map((parseUrl _).tupled).filter(_.isDefined).map(_.get)
-      }
-    }
+    def parsePrice(steamEntry: SteamEntry, suggestions: Seq[PartialPrice]): Option[PriceEntry] =
+      getWinner(steamEntry, suggestions).map(p => PriceEntry(steamEntry.steamId, p.name, FK.toString, p.link, BigDecimal(0)))
 
     for {
-      links <- getLinks
-      details <- Future.sequence(links.map(e => retriever(e.link.split(FK.toString)(1)).map(s => (e, s))))
+      complete <- Future.sequence(entries.map(e => getSuggestions(e.name, tables, retriever).map(s => (e, s))))
+      prices = complete.filter(p => p._2.nonEmpty).map((parsePrice _).tupled).filter(_.isDefined).map(_.get)
+      details <- Future.sequence(prices.map(e => retriever(e.link.split(FK.toString)(1)).map(s => (e, s))))
     } yield {
-      def getPrice(e: PriceEntry, page: String) = {
+      def fixPrice(e: PriceEntry, page: String) = {
         val parsed = Jsoup.parse(page)
         val price = parsed.getElementById("gameinfo").getElementsByClass("price").head.text().split(" ")(0)
         val activeContent = parsed.getElementById("content").getElementsByClass("active").head
@@ -107,7 +102,7 @@ object FKPricesFetcher {
         e.copy(link = link, price = BigDecimal(price))
       }
 
-      details.map((getPrice _).tupled)
+      details.map((fixPrice _).tupled)
     }
   }
 
